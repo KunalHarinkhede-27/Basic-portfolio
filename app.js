9ad82cb578dc9a42842c50754e4a89ceb4997031
kunal harinkhede <kunalharinkhede277@gmail.com>
2024-03-23 15:40:18 +0530

    my personal portfolio files

---
+++ app.js
@@ -0,0 +1,20 @@
+// let outer=document.querySelector("#ho");
+// outer.addEventListener("click",function ho(){
+//     event.preventDefault();
+//     window.scrollTo(0,0)
+// });
+// let outer1=document.querySelector("#ab"); 
+// outer1.addEventListener("click",function ab(){
+//     event.preventDefault();   
+//     window.scrollTo(0,550);
+// });
+// let outer2=document.querySelector("#po");
+// outer2.addEventListener("click",function po(){
+//     event.preventDefault();
+//     window.scrollTo(0,1130);
+// });
+// let outer3=document.querySelector("#cont");
+// outer3.addEventListener("click",function cont(){
+//     event.preventDefault();
+//     window.scrollTo(0,1780);
+// });
